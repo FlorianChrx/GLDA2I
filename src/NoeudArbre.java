aa0c7a93b85390c6de8ef2d4ca785bd6c546f739
Florian CHIRAUX <chirauxf@meleze24.iut-infobio.priv.univ-lille1.fr>
2020-09-21 12:00:34 +0200

    reprise projet arbres

--- src/NoeudArbre.java
+++ src/NoeudArbre.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class NoeudArbre {
@@ -63,15 +64,34 @@ public class NoeudArbre {
         this.chaine = question;
     }
 
+    String definir(String animal) {
+        if(droite == null && gauche == null) return chaine;
+        String resG = "";
+        String resD = "";
+        if(gauche != null) resG = gauche.definir(animal);
+        if(droite != null) resG = gauche.definir(animal);
+        if(!resG.contains(animal)) resG = "";
+        if(!resG.contains(animal)) resG = "";
+        return chaine + "=>" + resG + resD;
+    }
+
     public static void main(String[] args) {
         NoeudArbre n;
-        System.out.println(args.length);
-        if (args.length == 3) {
-            n = new NoeudArbre(args[0], args[1], args[2]);
-        } else {
-            n = new NoeudArbre("Est-ce un mammifère ?");
-            n.droite = new NoeudArbre("chien");
-            n.gauche = new NoeudArbre("poisson");
+        switch (args.length){
+            case 3:
+                System.out.println(Arrays.toString(args));
+                n = new NoeudArbre(args[0], args[1], args[2]);
+                break;
+            case 2:
+                if(args[0].equals("--definir")) {
+                    n = new NoeudArbre("Est-ce un mammifère ?");
+                    n.droite = new NoeudArbre("Est-ce un canidé ?", "chat", "chien");
+                    n.gauche = new NoeudArbre("Est-ce dangereux ?", "poisson", "requin");
+                    System.out.println(n.definir(args[1]));
+                    break;
+                }
+            default:
+                n = new NoeudArbre("Est-ce un mammifère ?", "chien", "poisson");
         }
         do {
             n.rechercherAnimal();
