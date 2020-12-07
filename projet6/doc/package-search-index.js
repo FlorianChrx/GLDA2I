2975e15cd8ca5b13a799e2973db41faa557b8b7c
FlorianChrx <florianchiro@hotmail.fr>
2020-12-07 18:24:15 +0100

    test

---
+++ projet6/doc/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"animaux"},{"l":"dictionnaire"},{"l":"employesAvecHeritage"},{"l":"employesSansHeritage"},{"l":"image"}]
